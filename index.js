4092adece5b8df7d78c577b18326ab1a8de0c0fd
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-25 18:10:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'personal token'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
